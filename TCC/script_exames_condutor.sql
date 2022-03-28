0bac557f198a6d3cdfadbdd66b5d20b9c4b39e39
Luiz Rodriguez Fantini <luizrfantini@gmail.com>
2022-03-28 18:23:24 -0300

    Update script_exames_condutor.sql

--- TCC/script_exames_condutor.sql
+++ TCC/script_exames_condutor.sql
@@ -13,3 +13,14 @@ from sa_dut.tb_processo p
 	left join sa_dut.tb_motivoprocesso mp using(numprocesso)
 	left join sa_dut.tb_exameprocesso ep using(numprocesso)
 where mp.codmot = 1 and ep.codtipoexame in (1,2,8,3,4,9) group by p.numprocesso 
+
+select p.numprocesso, 
+	count(rm) as examesmedicos, 
+	count(rt) as examesteoricos,
+	count(rp) as examespraticos 
+from sa_dut.tb_processo p 
+	left join sa_dut.tb_motivoprocesso mp using(numprocesso)
+	left join sa_dut.tb_resultadoteorico rt using(numprocesso)
+	left join sa_dut.tb_resultadopratico rp using(numprocesso)
+	left join sa_dut.tb_resultadomedico rm using(numprocesso)
+where mp.codmot = 1 and p.codsitprocesso = 99 group by p.numprocesso 
