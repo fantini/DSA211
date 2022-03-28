91ca42b63ee5668d93d58b9ddf48bbfc5a11f0c9
Luiz Rodriguez Fantini <luizrfantini@gmail.com>
2022-03-28 18:03:58 -0300

    Update script_exames_condutor.sql

--- TCC/script_exames_condutor.sql
+++ TCC/script_exames_condutor.sql
@@ -4,3 +4,12 @@ select
 	count(case when codtipoexame in (3) then 1 end) as examesteoricos,
 	count(case when codtipoexame in (4,9) then 1 end) as examespraticos
 from sa_dut.tb_exameprocesso where codtipoexame in (1,2,8,3,4,9) group by numprocesso 
+
+select p.numprocesso, 
+	count(case when ep.codtipoexame in (1,2,8) then 1 end) as examesmedicos, 
+	count(case when ep.codtipoexame in (3) then 1 end) as examesteoricos,
+	count(case when ep. codtipoexame in (4,9) then 1 end) as examespraticos 
+from sa_dut.tb_processo p 
+	left join sa_dut.tb_motivoprocesso mp using(numprocesso)
+	left join sa_dut.tb_exameprocesso ep using(numprocesso)
+where mp.codmot = 1 and ep.codtipoexame in (1,2,8,3,4,9) group by p.numprocesso 
