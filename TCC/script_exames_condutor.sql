7f59ffb6a75c61e83574599731230cb0ae00eff4
Luiz Rodriguez Fantini <luizrfantini@gmail.com>
2022-03-28 23:32:24 -0300

    Update script_exames_condutor.sql

--- TCC/script_exames_condutor.sql
+++ TCC/script_exames_condutor.sql
@@ -1,28 +1,19 @@
-select 
-	numprocesso, 
-	count(case when codtipoexame in (1,2,8) then 1 end) as examesmedicos, 
-	count(case when codtipoexame in (3) then 1 end) as examesteoricos,
-	count(case when codtipoexame in (4,9) then 1 end) as examespraticos
-from sa_dut.tb_exameprocesso where codtipoexame in (1,2,8,3,4,9) group by numprocesso 
-
-select p.numprocesso, 
-	count(case when ep.codtipoexame in (1,2,8) then 1 end) as examesmedicos, 
-	count(case when ep.codtipoexame in (3) then 1 end) as examesteoricos,
-	count(case when ep. codtipoexame in (4,9) then 1 end) as examespraticos 
+select p.numprocesso, u.numcpf,
+	count(distinct rm) as examesmedicos, 
+	count(distinct rt) as examesteoricos,
+	count(distinct rp) as examespraticos 
 from sa_dut.tb_processo p 
-	left join sa_dut.tb_motivoprocesso mp using(numprocesso)
-	left join sa_dut.tb_exameprocesso ep using(numprocesso)
-where mp.codmot = 1 and ep.codtipoexame in (1,2,8,3,4,9) group by p.numprocesso 
-
-select p.numprocesso, 
-	count(rm) as examesmedicos, 
-	count(rt) as examesteoricos,
-	count(rp) as examespraticos 
-from sa_dut.tb_processo p 
+	left join sa_dut.tb_usuario u using(numprocesso)	
 	left join sa_dut.tb_motivoprocesso mp using(numprocesso)
 	left join sa_dut.tb_resultadoteorico rt using(numprocesso)	
 	left join sa_dut.tb_agendamentopratico ap using(numprocesso)
-	left join sa_dut.tb_resultadopratico rp using(ap.numpauta)
+	left join sa_dut.tb_resultadopratico rp on ap.numpauta = rp.numpauta
 	left join sa_dut.tb_agendamentomedico am using(numprocesso)
-	left join sa_dut.tb_resultadomedico rm using(am.numpauta)
-where mp.codmot = 1 and p.codsitprocesso = 99 group by p.numprocesso 
+	left join sa_dut.tb_resultadomedico rm on am.numpauta = rm.numpauta
+where 
+	mp.codmot = 1 
+	and p.codsitprocesso = 99 
+	and rp.codresultado in (1,2)
+	and rm.codresultado in (1,2,3,4,5,7)
+	and rt.codresultado in (1,2)
+group by p.numprocesso, u.numcpf
